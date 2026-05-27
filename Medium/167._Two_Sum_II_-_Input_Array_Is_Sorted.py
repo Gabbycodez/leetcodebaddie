2e6444a9a4652213da28899a8f4eb3ebad273b56
Gabbycodez <115847115+Gabbycodez@users.noreply.github.com>
2026-05-27 19:20:13 -0400

    167. Two Sum II - Input Array Is Sorted (Medium) - 2026-05-27

---
+++ Medium/167._Two_Sum_II_-_Input_Array_Is_Sorted.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start = 0
+        end = len(numbers) - 1
+
+        while start < end: # 0 , 3
+
+            total = numbers[start] + numbers[end] # total = 2 + 15
+
+            if total == target: 
+                return [start + 1, end + 1]
+
+            elif total < target:#17
+                start += 1
+            else:
+                end -= 1
+
+        return [-1, -1]
+End
+ENd
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - 142. Linked List Cycle II | Medium | 2026-05-19
 - 141. Linked List Cycle | Easy | 2026-05-20
 - 234. Palindrome Linked List | Easy | 2026-05-24
+- 167. Two Sum II - Input Array Is Sorted | Medium | 2026-05-27
